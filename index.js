40d710939289d82c2b2cd827f2b302b4f395b8cf
siddhanth98 <svenka42@uic.edu>
2020-01-19 22:43:11 -0600

    Creates and stores event change details in a javascript object. Convert this to a json string and store it in a json file

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 
 var slidUp = false;
 var xml = new XMLSerializer();
-var paraInitial = document.querySelector("#divOriginal p").innerHTML;
+var paraInitial = document.querySelector("#divOriginal p");
+var paraInitialContent = paraInitial.innerText;
 
 /* Slide Up Change */
 $("#divOriginal button").on("click", function() {
+    var thisButton = $(this);
+    console.log(thisButton);
+
     if(!slidUp) {
         /* Expected Change */
         $("#divOriginal p").slideUp();
@@ -20,17 +24,34 @@ $("#divOriginal button").on("click", function() {
         slidUp = false;
 
         setTimeout(function() {
-            var paraNew = document.querySelector("#divOriginal p").innerHTML;
-            if(paraInitial !== paraNew) {
-                console.log("Changed");
-                setTimeout(function() {
+            var paraNew = document.querySelector("#divOriginal p");
+            var paraNewContent = paraNew.innerText;
+
+            if(paraInitialContent !== paraNewContent) {
+                var paragraphObject = {
+                    class : paraNew.classList,
+                    id : paraNew.id,
+                    oldContent : paraInitialContent,
+                    newContent : paraNewContent,
+                    eventType : "click",
+                    eventTrigger : thisButton.text()
+                };
+
+                alert(paragraphObject.eventType + " on " + paragraphObject.eventTrigger + " changed \n\n" +
+                    paragraphObject.oldContent + " \n to \n" + paragraphObject.newContent);
+
+                /* Store changes detected in an element in a json file with element details like class names, id, old content, modified
+                * content, events triggering the changes and so on. */
+
+
+                /*setTimeout(function() {
                     $("body").append("<script type=\"text/javascript\">\n" +
                         "            var blob = new Blob([\"File Changed \"], {type: \"text/plain;charset=utf-8\"});\n" +
                         "            saveAs(blob, \"fileChange.txt\");\n" +
                         "    </script>");
-                }, 500);
+                }, 500);*/
             }
-        }, 0);
+        }, 1000);
 
     }
 });
